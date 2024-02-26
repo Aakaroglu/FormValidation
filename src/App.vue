a3278b8454110b39352a9e48369f00580b625880
Aakaroglua <136961780+Aakaroglua@users.noreply.github.com>
2024-02-26 15:48:38 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import FormValidation from './components/FormValidation.vue';
+</script>
+
+
+<template>
+
+  <FormValidation/>
+
+</template>
---
+++ src/components/FormValidation.vue
@@ -0,0 +1,104 @@
+
+<script setup>
+import { ref ,computed } from "vue";
+
+
+
+const formData = ref({
+
+    name1:'',
+    email: "",
+    password: ""
+})
+
+const isNameValid = computed(() => formData.value.name1.trim() !== '')
+const isEmailValid = computed(() => /^[^\&@]+@[^\&@]+\.[^\&@]+$/.test(formData.value.email))
+const isPasswordValid = computed(() => formData.value.password.length>= 8)
+const isFormValid = computed (() => isNameValid.value && isEmailValid.value && isPasswordValid.value)
+
+
+const submitForm = () => {
+    if (isFormValid.value) {
+        // A lot of logic
+        console.log('Form Submitted' , formData.value)
+    }
+    else {
+        console.log('Form is invalid. Please check the fields')
+    }
+}
+
+</script>
+
+<template>
+
+    <div>
+        <form @submit.prevent="submitForm" class="custom-form">
+        <div class="form-group">
+            <label for="name1">Name: </label>
+            <input v-model="formData.name1" type="text" id="name1">
+            <span v-if="!isNameValid" class="error">Name is required</span>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email: </label>
+            <input v-model="formData.email" type="email" id="email">
+            <span v-if="!isEmailValid" class="error">Please enter a valid email address</span>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password: </label>
+            <input v-model="formData.password" type="password" id="password">
+            <span v-if="!isPasswordValid" class="error">Password must be at least 8 characters </span>
+        </div>
+
+        <button type="submit" class="submit-button">Submit</button>
+
+
+        </form>
+    </div>
+
+</template>
+
+<style>
+
+.custom-form{
+    max-width: 400px;
+    margin: 0 auto;
+}
+
+.form-group{
+    margin-bottom: 20px;
+}
+
+label{
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+input {
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+.error{
+    color: #e74c3c;
+    font-size: 14px;
+    margin-top: 5px;
+}
+
+.submit-button{
+    padding: 10px 15px;
+    font-size: 16px;
+    background-color: #3498db;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+
+</style>
